34570fa9d923db37c8a22854d5735a544a342ff0
Othmane Harraq <tur46520@temple.edu>
2025-08-29 20:00:27 -0400

    Modified the file by adding comments

--- random.c
+++ random.c
@@ -1,3 +1,14 @@
+/* 
+Name: Othmane Harraq
+TUID: 916315325
+Section: 1
+Project: Project Git
+Program: This program returns a random 
+alphabet letter, which is used in the
+print_random.c function to print a 7 letter
+word randomly 
+*/
+
 #include <stdlib.h>
 #include <time.h>
 
