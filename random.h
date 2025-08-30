427c900aaf51e3f204ddfd469c7271d4f0c6177c
Othmane Harraq <tur46520@temple.edu>
2025-08-29 20:21:21 -0400

    Added the header file

---
+++ random.h
@@ -0,0 +1,14 @@
+/* 
+Name: Othmane Harraq
+TUID: 916315325
+Section: 1
+Project: Project Git
+Program: This is a header file 
+*/
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+char randchar();  
+
+#endif
